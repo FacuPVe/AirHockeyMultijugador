544adb8882ce634f01176d701fb8db9aff6caaa4
FacuPVe <facundopaiver@monlau.com>
2025-11-09 16:54:36 +0100

    first commit

---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private string playerPrefabName = "PlayerPrefab";
+
+    [SerializeField]
+    private Transform spawnPoint;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master");
+        PhotonNetwork.JoinRandomOrCreateRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Player joined Room");
+        // Usar la posición del Transform 'spawnPoint' si está asignado
+        Vector3 spawnPosition = spawnPoint != null ? spawnPoint.position : Vector3.up * 2f;
+        // Instanciar Prefab
+        PhotonNetwork.Instantiate(this.playerPrefabName, spawnPosition, Quaternion.identity);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
