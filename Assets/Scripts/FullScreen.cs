9aee64b24d1bc66ee90fd44e4b2dac135115b4bd
FacuPVe <facundopaiver@monlau.com>
2025-11-27 16:16:32 +0100

    feat: poder poner el juego en modo ventana o completa pulsando F11

---
+++ Assets/Scripts/FullScreen.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class FullScreen : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+
+            Debug.Log("Fullscreen activated: " + Screen.fullScreen);
+        }
+    }
+}
